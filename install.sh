03b487f488f627eec55433e2c7e60fb70d8c7cb0
Paul <paul.nikiforov@gmail.com>
2019-09-29 17:13:51 +0900

    save

--- install.sh
+++ install.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-dotfiles_dir=$(pwd)
-usr_bin=~/bin
+# dotfiles_dir=$(pwd)
+dotfiles_dir=$(cd `dirname $0` && pwd)
 
 if [ ! -d ~/bin ]; then
     mkdir ~/bin
 fi
+
 cd ~/bin
 curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
 chmod u+x nvim.appimage
 ln -sf nvim.appimage nvim
 ln -sf nvim.appimage vim
 ln -sf $dotfiles_dir/nvim ~/.config
+
